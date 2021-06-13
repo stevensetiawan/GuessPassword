e9a4e0e0466c8253b1430d5ecf44d61997b41f1d
stevensetiawan <58681831+stevensetiawan@users.noreply.github.com>
2021-06-13 17:56:09 +0700

    Add files via upload
    update

--- GuessPassword.js
+++ GuessPassword.js
@@ -5,29 +5,28 @@ const PassPossibility = (range1, range2) => {
     let correctPassAmount = 0
     let adjacent_digit = false
 
-   loopCounter: for(counter=range1; counter <=range2; counter+=1){
-       password= counter.toString();
-       passlength= password.length
-       adjacent_digit= false
-       zeroDigit= ''
-       if(passlength < 6){
-        for(let len= 6; len > passlength; len -= 1){
-              zeroDigit +='0'
-        }  
-            password = zeroDigit + password
-      }
-        
+    loopCounter: for (counter = range1; counter <= range2; counter += 1) {
+        password = counter.toString();
         passlength = password.length
-        
-        for(i=0; i<passlength-1; i++){
-            if(password[i] > password[i+1]){
-                continue loopCounter
+        adjacent_digit = false
+        zeroDigit = ''
+        if (passlength < 6) {
+            for (let len = 6; len > passlength; len -= 1) {
+                zeroDigit += '0'
             }
-            else if(password[i] == password[i+1]){
-                adjacent_digit=true
+            password = zeroDigit + password
+        }
+
+        passlength = password.length
+
+        for (i = 0; i < passlength - 1; i++) {
+            if (password[i] > password[i + 1]) {
+                continue loopCounter
+            } else if (password[i] == password[i + 1]) {
+                adjacent_digit = true
             }
         }
-        if(adjacent_digit==true){
+        if (adjacent_digit == true) {
             correctPassAmount += 1
         }
         // console.log(password, correctPassAmount, " this is password & correct pass amount ")
